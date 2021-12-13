d48b525412698fa5898ee7ab2d110b19a03dea78
Yulka <barsetca@rambler.ru>
2021-12-13 15:04:54 +0300

    hw lesson2 - итоговый вариант

--- src/main/java/ru/geekbrains/lessons/lesson2/HomeWork2.java
+++ src/main/java/ru/geekbrains/lessons/lesson2/HomeWork2.java
@@ -1,5 +1,7 @@
 package ru.geekbrains.lessons.lesson2;
 
+import java.util.Arrays;
+
 public class HomeWork2 {
 
 
@@ -9,24 +11,13 @@ public class HomeWork2 {
         String[] producers = {"Lenovi", "Asun", "MacNote", "Eser", "Xamiou"};
         Supplier supplier = new Supplier(param, producers);
 
-        Notebook[] notebooks = supplier.giveNotebooks(10);
-//        for (int i = 0; i < notebooks.length - 1; i++) {
-//            System.out.println(notebooks[i]);
-//        }
-        // тестовые данные
-//        Notebook[]notebooks = new Notebook[10];
-//        notebooks[0] = new Notebook("Xamiou",  16, 1600);
-//        notebooks[1] = new Notebook("Eser",  16, 1600);
-//        notebooks[2] = new Notebook("Eser",  4, 850);
-//        notebooks[4] = new Notebook("Asun",  4, 850);
-//        notebooks[5] = new Notebook("MacNote",  16, 1600);
-//        notebooks[3] = new Notebook("Lenovi",  24, 1350);
-//        notebooks[6] = new Notebook("Xamiou",  8, 1600);
-//        notebooks[7] = new Notebook("Lenovi",  4, 850);
-//        notebooks[8] = new Notebook("Lenovi",  20, 1350);
-//        notebooks[9] = new Notebook("Lenovi",  4, 850);
 
-        supplier.sort(notebooks);
+        Notebook[] notebooks = supplier.getNotebooks(20);
+        System.out.println(Arrays.asList(notebooks));
+        //     Notebook[] notebooks = supplier.getNotebooks(); test
 
+        supplier.sortInsert();
+        System.out.println(Arrays.asList(notebooks));
     }
 }
+
--- src/main/java/ru/geekbrains/lessons/lesson2/Supplier.java
+++ src/main/java/ru/geekbrains/lessons/lesson2/Supplier.java
@@ -2,7 +2,6 @@ package ru.geekbrains.lessons.lesson2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Random;
 
 public class Supplier {
@@ -15,8 +14,9 @@ public class Supplier {
     int stepDDR;
     ArrayList<String> titles;
     Random random;
+    Notebook[] notebooks;
 
-    public Supplier(int[] param, String [] producers) {
+    public Supplier(int[] param, String[] producers) {
         titles = new ArrayList<>(Arrays.asList(producers));
         this.minPrice = param[0];
         this.maxPrice = param[1];
@@ -24,7 +24,7 @@ public class Supplier {
         this.minDDR = param[3];
         this.maxDDR = param[4];
         this.stepDDR = param[5];
-        Random random;
+
     }
 
     private int generated(int max, int min, int step) {
@@ -32,11 +32,26 @@ public class Supplier {
         int diff1 = (max - min) / step;
         int i = random.nextInt(diff1 + 1);
         return min + i * step;
-
     }
 
-    public Notebook[] giveNotebooks(int count) {
-        Notebook[] notebooks = new Notebook[count];
+//    public Notebook[] getNotebooks(){
+    // тестовые данные
+//       notebooks = new Notebook[10];
+//        notebooks[0] = new Notebook("Xamiou",  16, 1600);
+//        notebooks[1] = new Notebook("Eser",  16, 1600);
+//        notebooks[2] = new Notebook("Eser",  4, 850);
+//        notebooks[4] = new Notebook("Asun",  4, 850);
+//        notebooks[5] = new Notebook("MacNote",  16, 1600);
+//        notebooks[3] = new Notebook("Lenovi",  24, 1350);
+//        notebooks[6] = new Notebook("Xamiou",  8, 1600);
+//        notebooks[7] = new Notebook("Lenovi",  4, 850);
+//        notebooks[8] = new Notebook("Lenovi",  20, 1350);
+//        notebooks[9] = new Notebook("Lenovi",  4, 850);
+//        return notebooks;
+//    }
+
+    public Notebook[] getNotebooks(int count) {
+        notebooks = new Notebook[count];
         random = new Random();
 
         for (int i = 0; i < count; i++) {
@@ -49,23 +64,45 @@ public class Supplier {
         return notebooks;
     }
 
-    public void sort (Notebook[] notebooks) {
-        Comparator<Notebook> comparator = (o1, o2) -> {
-            int value = o1.getPrice().compareTo(o2.getPrice());
-            if (value == 0) {
-                int value2 = o1.getDdr().compareTo(o2.getDdr());
-                if (value2 == 0) {
-                    int value3 = titles.indexOf(o1.getTitle()) - titles.indexOf(o2.getTitle());
-                    return Integer.compare(value3, 0);
+//    public void sort() {
+//        Arrays.sort(notebooks, (o1, o2) -> {
+//            int value = o1.getPrice().compareTo(o2.getPrice());
+//            if (value == 0) {
+//                int value2 = o1.getDdr().compareTo(o2.getDdr());
+//                if (value2 == 0) {
+//                    int value3 = titles.indexOf(o1.getTitle()) - titles.indexOf(o2.getTitle());
+//                    return Integer.compare(value3, 0);
+//                }
+//                return value2;
+//            }
+//            return value;
+//        });
+//    }
 
-                }
-                return value2;
+    public void  sortInsert() {
+        int index;
+        for (int out = 1; out < notebooks.length; out++) {
+            Notebook notebook = notebooks[out];
+            index = out;
+            while (index > 0 && needToChange(notebooks[index - 1], notebook)) {
+                notebooks[index] = notebooks[index - 1];
+                --index;
             }
-            return value;
-        };
+            notebooks[index] = notebook;
+        }
+    }
 
-        ArrayList<Notebook> list = new ArrayList<>(Arrays.asList(notebooks));
-        list.sort(comparator);
- // System.out.println(list);
+    private boolean needToChange(Notebook o1, Notebook o2) {
+        int value = o1.getPrice().compareTo(o2.getPrice());
+        if (value == 0) {
+            int value2 = o1.getDdr().compareTo(o2.getDdr());
+            if (value2 == 0) {
+                int value3 = titles.indexOf(o1.getTitle()) - titles.indexOf(o2.getTitle());
+                return value3 > 0;
+            }
+            return value2 > 0;
+        }
+        return value > 0;
     }
 }
+
